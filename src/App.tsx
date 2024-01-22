42f7c472291682982776e1a290af8b48476f51c2
Aleksey Nogay <leshkanogay@gmail.com>
2024-01-22 17:35:55 +0500

    fixed and installed tailwind with config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from "react"
+import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { MainPage } from "./styled-components/Pages/MainPage/MainPage";
-import { ProductPage } from "./styled-components/Pages/ProductPage/ProdictPage";
+import { MainPage } from "./Pages/MainPage/MainPage";
 
 const routerConfig = createBrowserRouter([
   {
     path: "/",
-    element: <MainPage/>
+    element: <MainPage />,
   },
-  {
-    path: "/Product",
-    element: <ProductPage/>
-  }
 ]);
 
 const App: React.FC = () => {
   return (
-          <div className="App"><RouterProvider router={routerConfig}/></div>
-          // <h1>Hello</h1>
+    <div className="App">
+      <RouterProvider router={routerConfig} />
+    </div>
+    // <h1>Hello</h1>
   );
 };
 
-
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GlobalStyle } from './styled-components/globalStyle'
-import App from './App'
-// import App from './App.tsx'
-// import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyle } from "./styled-components/globalStyle";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App/>
-  </React.StrictMode>,
-)
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
